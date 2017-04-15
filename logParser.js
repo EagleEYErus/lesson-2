b509a1f325400fcfda09bd190bb7b6d835670a34
EagleEYErus <alex_afsoc@mail.ru>
2017-04-15 20:03:11 +0300

    Update logParser.js

--- logParser.js
+++ logParser.js
@@ -3,7 +3,16 @@ const fs = require('fs');
 const WordTable = require('word-table');
 
 const argv = argvParser(process.argv.slice(2));
-const p = '\\[%{DATA:time}\\] %{WORD:winStatus} Побед: %{INT:wins} Поражений: %{INT:looses} WinStreak: %{INT:winStreak} LooseStreak: %{INT:looseStreak} Всего сыграно игр: %{INT:games} Максимальный WinStreak: %{INT:maxWinStreak} Максимальный LooseStreak: %{INT:maxLooseStreak} WinRate: %{WORD:winRate}';
+const p = '\\[%{DATA:time}\\] ' + 
+		'%{WORD:winStatus} ' + 
+		'Побед: %{INT:wins} ' + 
+		'Поражений: %{INT:looses} ' + 
+		'WinStreak: %{INT:winStreak} ' + 
+		'LooseStreak: %{INT:looseStreak} ' +
+		'Всего сыграно игр: %{INT:games} ' + 
+		'Максимальный WinStreak: %{INT:maxWinStreak} ' + 
+		'Максимальный LooseStreak: %{INT:maxLooseStreak} ' + 
+		'WinRate: %{WORD:winRate}';
 var dataArray = [];
 
 fs.readFile(argv.path, 'utf8', (err, data) => {
@@ -17,8 +26,10 @@ fs.readFile(argv.path, 'utf8', (err, data) => {
 	var header = ['Win Status', 'Total Wins', 'Total Looses', 'Games', 'WinRate', 'WinStreak', 'LooseStreak'];
 	var body = [];
 	dataArray.forEach((item, i) => {
-		var array = [item.winStatus, item.wins, item.looses, item.games, item.winRate + ' %', item.maxWinStreak, item.maxLooseStreak];
-		body.push(array);
+		if (item !== null) {
+			var array = [item.winStatus, item.wins, item.looses, item.games, item.winRate + ' %', item.maxWinStreak, item.maxLooseStreak];
+			body.push(array);
+		}
 	});
 	const wt = new WordTable(header, body);
 	console.log(wt.string());
