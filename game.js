7d2806586817f2c12fd604d253b9503704637938
EagleEYErus <alex_afsoc@mail.ru>
2017-04-15 19:44:02 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,104 @@
+const argvParser = require('minimist');
+const fs = require('fs');
+const readline = require('readline');
+
+const argv = argvParser(process.argv.slice(2));
+var maxWinStreak = 0;
+var maxLooseStreak = 0;
+var wins = 0;
+var looses = 0;
+var games = 0;
+var winStreak = 0;
+var looseStreak = 0;
+var wins = 0;
+var looses = 0;
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const game = name => {
+
+	const gameEnum = {
+		HEADS: 1,
+		TAILS: 2
+	};
+
+	const resultEnum = {
+		WIN: 1,
+		LOOSE: 0,
+		ERROR: -1
+	};
+
+	const number = random(1, 3);
+
+	switch(name) {
+		case 'Орел':
+			if (number === gameEnum.HEADS) {
+				return resultEnum.WIN;
+			} else {
+				return resultEnum.LOOSE;
+			}
+		case 'Решка':
+			if (number === gameEnum.TAILS) {
+				return resultEnum.WIN;
+			} else {
+				return resultEnum.LOOSE;
+			}
+		case 'exit':
+			rl.close();
+			break;
+		default: 
+			return resultEnum.ERROR;
+	}
+}
+
+var writeLog = isWin => {
+
+	log = '[' + new Date().toLocaleString() + '] ';
+
+	if (isWin) {
+		log += 'Победа';
+	} else {
+		log += 'Поражение';
+	}
+
+	log += ' Побед: ' + wins + 
+		' Поражений: ' + looses +  
+		' WinStreak: ' + winStreak +  
+		' LooseStreak: ' + looseStreak  +
+		' Всего сыграно игр: ' + games +
+		' Максимальный WinStreak: ' + maxWinStreak +
+		' Максимальный LooseStreak: ' + maxLooseStreak + 
+		' WinRate: ' + (wins/games)*100 + '%\n\r';
+
+	fs.appendFile(argv.path, log, err => {
+		if (err) console.log(err.message);
+	});
+};
+
+rl.on('line', cmd => {
+	const result = game(cmd);
+	if (result) {
+		games++;
+		winStreak++;
+		wins++;
+		maxLooseStreak = looseStreak;
+		looseStreak = 0;
+		console.log('Вы победили!');
+		writeLog(1);
+	} else if (result === 0) {
+		games++;
+		looseStreak++;
+		looses++;
+		maxWinStreak = winStreak;
+		winStreak = 0;
+		console.log('Вы проиграли!');
+		writeLog(0);
+	} else if (result === -1 ) console.log('Неккоректный запрос');
+});
---
+++ logParser.js
@@ -0,0 +1,25 @@
+const argvParser = require('minimist');
+const fs = require('fs');
+const WordTable = require('word-table');
+
+const argv = argvParser(process.argv.slice(2));
+const p = '\\[%{DATA:time}\\] %{WORD:winStatus} Побед: %{INT:wins} Поражений: %{INT:looses} WinStreak: %{INT:winStreak} LooseStreak: %{INT:looseStreak} Всего сыграно игр: %{INT:games} Максимальный WinStreak: %{INT:maxWinStreak} Максимальный LooseStreak: %{INT:maxLooseStreak} WinRate: %{WORD:winRate}';
+var dataArray = [];
+
+fs.readFile(argv.path, 'utf8', (err, data) => {
+	const logsArray = data.split('\n\r');
+	const patterns = require('node-grok').loadDefaultSync();
+	const pattern = patterns.createPattern(p);
+	logsArray.forEach((item, i) => {
+		dataArray.push(pattern.parseSync(item));
+	});
+
+	var header = ['Win Status', 'Total Wins', 'Total Looses', 'Games', 'WinRate', 'WinStreak', 'LooseStreak'];
+	var body = [];
+	dataArray.forEach((item, i) => {
+		var array = [item.winStatus, item.wins, item.looses, item.games, item.winRate + ' %', item.maxWinStreak, item.maxLooseStreak];
+		body.push(array);
+	});
+	const wt = new WordTable(header, body);
+	console.log(wt.string());
+});
